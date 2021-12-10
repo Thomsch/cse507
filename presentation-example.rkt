27e7fdc251ac9ae808f86e76d4823da3b77a7967
Thomas Schweizer <tschweiz@cs.washington.edu>
2021-12-09 21:57:36 -0800

    Update Fasenra example

--- presentation-example.rkt
+++ presentation-example.rkt
@@ -6,9 +6,9 @@
   (make-database
    #:drugs ; drug: name, patient requirements, properties
    (list
-    (drug 'Fasenra  '() '())
-    (drug 'Vitamax  '() '())
-    (drug 'UltraVitamin  '() '())
+    (drug 'Fasenra  (list (older-than 5)(any-allergy 'Cephalosporin)) '(Diuretic))
+    (drug 'Vitamax  (list (no-allergy 'Folate)) '(Multivitamin)) ; Cannot be allergic to Folate. Drug is a multivitamin.
+    (drug 'UltraVitamin  (list (no-allergy 'Folate)) '(Biological)) ; Cannot be allergic to Folate. Drug is biological.
     )
 
    #:conflicts ; conflict: two drug names and a condition
@@ -19,11 +19,11 @@
 
    #:treatments ; treatment: ailments treated, patient requirements, drug formula
    (list
-    (treatment '(Asthma) (list (older-than 10)) '(Fasenra)) ; Drug Fasenra treats Asthma if patient is over age 10.
+    (treatment '(Asthma) (list (younger-than 70)) '(Fasenra)) ; Drug Fasenra treats Asthma if patient is younger than 70
     )))
 
 (define (example)
-  (define jessie (patient 26 '(Cephalosporin) '(Asthma))) ; Jessie is 26, is allergic to Cephalosporin, and has Asthma
+  (define jessie (patient 26 '(Cephalosporin) '(Asthma))) ; Jessie is 26, is allergic to Cephalosporin, and has Asthma.
   (define baseline-prescription '(Fasenra))
   (define prescription-1 '(Fasenra Vitamax)) ; Conflict because Jessie is allergic to Cephalosporin.
   (define prescription-2 '(Fasenra UltraVitamin)) ; No conflict! Jessie can use this!
